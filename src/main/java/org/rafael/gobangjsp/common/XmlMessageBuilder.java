cafe747a665a48b36880a285d885bb41d8288859
Rafael-LN <rafaelnunes3092@gmail.com>
2025-07-05 03:07:39 +0100

    :rocket: Add XML message handling classes and user profile data structure

---
+++ src/main/java/org/rafael/gobangjsp/common/XmlMessageBuilder.java
@@ -0,0 +1,105 @@
+package org.rafael.gobangjsp.common;
+
+public class XmlMessageBuilder {
+
+    
+    private static String wrapWithMessage(String contentXml) {
+        return "<message>" + contentXml + "</message>";
+    }
+
+    public static String buildLoginRequest(String username, String password) {
+        String content = "<loginRequest>" +
+                "<username>" + username + "</username>" +
+                "<password>" + password + "</password>" +
+                "</loginRequest>";
+        return wrapWithMessage(content);
+    }
+
+    public static String buildRegisterRequest(String username, String password, int age, String nationality, String photoPath) {
+        String content = "<registerRequest>" +
+                "<username>" + username + "</username>" +
+                "<password>" + password + "</password>" +
+                "<age>" + age + "</age>" +
+                "<nationality>" + nationality + "</nationality>" +
+                "<photo>" + photoPath + "</photo>" +
+                "</registerRequest>";
+        return wrapWithMessage(content);
+    }
+
+    public static String buildUpdateProfileRequest(String username, String photoBase64) {
+        String content = "<updateProfileRequest>" +
+                "<username>" + username + "</username>" +
+                "<photo>" + photoBase64 + "</photo>" +
+                "</updateProfileRequest>";
+        return wrapWithMessage(content);
+    }
+
+    public static String buildResponse(String status, String message, String operation) {
+        String content = "<response>" +
+                "<status>" + status + "</status>" +
+                "<message>" + message + "</message>" +
+                "<operation>" + operation + "</operation>"+
+                "</response>";
+        return wrapWithMessage(content);
+    }
+
+    public static String buildAuthResponse(String status, String message, String operation, String username, String photoBase64, int age, String nationality, int wins, int losses, long timePlayed) {
+        String content = "<response>" +
+                "<status>" + status + "</status>" +
+                "<message>" + message + "</message>" +
+                "<operation>"+ operation +"</operation>" +
+                "<username>" + username + "</username>" +
+                "<photo>" + (photoBase64 != null ? photoBase64 : "") + "</photo>" +
+                "<age>" + age + "</age>" +
+                "<nationality>" + nationality + "</nationality>" +
+                "<wins>" + wins + "</wins>" +
+                "<losses>" + losses + "</losses>" +
+                "<timePlayed>" + timePlayed + "</timePlayed>" +
+                "</response>";
+        return wrapWithMessage(content);
+    }
+
+    public static String buildFindMatchRequest(String username) {
+        String content = "<findMatch>" +
+                "<username>" + username + "</username>" +
+                "</findMatch>";
+        return wrapWithMessage(content);
+    }
+
+    public static String buildCancelMatchRequest(String username) {
+        String content = "<cancelMatch>" +
+                "<username>" + username + "</username>" +
+                "</cancelMatch>";
+        return wrapWithMessage(content);
+    }
+
+    public static String buildGameStart(String player, String opponent, boolean firstToPlay) {
+        String content = "<gameStart>" +
+                "<player>" + player + "</player>" +
+                "<opponent>" + opponent + "</opponent>" +
+                "<firstToPlay>" + firstToPlay + "</firstToPlay>" +
+                "</gameStart>";
+        return wrapWithMessage(content);
+    }
+
+    public static String buildMoveRequest(String username, int row, int col) {
+        String content = "<move>" +
+                "<player>" + username + "</player>" +
+                "<row>" + row + "</row>" +
+                "<col>" + col + "</col>" +
+                "</move>";
+        return wrapWithMessage(content);
+    }
+
+    public static String buildGameEnd(String winner, String reason, String message) {
+        String content = "<gameEnd>";
+        if (winner != null && !winner.isEmpty()) {
+            content += "<winner>" + winner + "</winner>";
+        }
+        content += "<reason>" + reason + "</reason>";
+        content += "<message>" + message + "</message>";
+        content += "</gameEnd>";
+        return wrapWithMessage(content);
+    }
+}
+
---
+++ src/main/java/org/rafael/gobangjsp/common/XmlMessageReader.java
@@ -0,0 +1,78 @@
+package org.rafael.gobangjsp.common;
+
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.*;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.*;
+
+public class XmlMessageReader {
+
+    public static Document parseXml(String xml) throws Exception {
+        DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        InputSource is = new InputSource(new StringReader(xml));
+        return builder.parse(is);
+    }
+
+    // Obtém o elemento filho principal da mensagem (payload)
+    public static Element getPayloadElement(Document doc) {
+        NodeList children = doc.getDocumentElement().getChildNodes();
+        for (int i = 0; i < children.getLength(); i++) {
+            Node node = children.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                return (Element) node;
+            }
+        }
+        return null;
+    }
+
+    // Devolve o nome do payload (ex: loginRequest, gameStart, etc)
+    public static String getPayloadType(Document doc) {
+        Element payload = getPayloadElement(doc);
+        return payload != null ? payload.getNodeName() : null;
+    }
+
+    // Obtém o valor de texto de uma tag dentro do payload (sem prefixos)
+    public static String getTextValue(Element element, String tagName) {
+        NodeList list = element.getChildNodes();
+        for (int i = 0; i < list.getLength(); i++) {
+            Node node = list.item(i);
+            if (node.getNodeType() == Node.ELEMENT_NODE) {
+                Element el = (Element) node;
+                if (tagName.equals(el.getNodeName())) {
+                    return el.getTextContent();
+                }
+            }
+        }
+        return null;
+    }
+
+    // Métodos auxiliares para obter valores a partir do payload
+    public static int getIntValue(Element element, String tagName) {
+        String value = getTextValue(element, tagName);
+        return value != null ? Integer.parseInt(value) : -1;
+    }
+
+    public static boolean getBooleanValue(Element element, String tagName) {
+        String value = getTextValue(element, tagName);
+        return Boolean.parseBoolean(value);
+    }
+
+    // Valida um XML recebido contra um XSD
+    public static boolean validateXml(String xml, String xsdPath) {
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+            Schema schema = factory.newSchema(new File(xsdPath));
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new StringReader(xml)));
+            return true;
+        } catch (Exception e) {
+            System.err.println("Erro de validação XML: " + e.getMessage());
+            return false;
+        }
+    }
+}
